28a133397c501ab7e2dd5e727d6585437b6d243a
osanaikoutarou <osanaikoutarou+github@gmail.com>
2018-10-21 08:16:58 +0900

    first commit

---
+++ SwiftCalendarSample/MonthCalendar/DayCell/DayCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  DayCollectionViewCell.swift
+//  SwiftCalendarSample
+//
+//  Created by 長内幸太郎 on 2018/10/20.
+//  Copyright © 2018年 長内幸太郎. All rights reserved.
+//
+
+import UIKit
+
+class DayCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ SwiftCalendarSample/Utility/DateUtility.swift
@@ -0,0 +1,13 @@
+//
+//  DateUtility.swift
+//  SwiftCalendarSample
+//
+//  Created by 長内幸太郎 on 2018/10/21.
+//  Copyright © 2018年 長内幸太郎. All rights reserved.
+//
+
+import UIKit
+
+class DateUtility: NSObject {
+
+}
